a49ef9401fd1cc084ec3053e7ac84c76da614438
KI-Experten <ki.experten@gmail.com>
2024-05-18 08:29:41 +0000

    update build dist

---
+++ dist/features/popup/components/DisclaimerPopup.d.ts
@@ -0,0 +1,7 @@
+export type DisclaimerPopupProps = {
+    isOpen?: boolean;
+    onAccept?: () => void;
+    onDecline?: () => void;
+};
+export declare const DisclaimerPopup: (props: DisclaimerPopupProps) => import("solid-js").JSX.Element;
+//# sourceMappingURL=DisclaimerPopup.d.ts.map
